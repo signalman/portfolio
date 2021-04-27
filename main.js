92cbf5a71d29ef7bb5445d6638ec4074de75d5d9
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-04-27 19:01:32 +0900

    js navbar menu click

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight= navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=>{
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY>navbarHeight){
         navbar.classList.add('navbar--dark');
     
@@ -13,3 +11,13 @@ document.addEventListener('scroll', ()=>{
         navbar.classList.remove('navbar--dark');
     }
 });
+
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    console.log(event.target.dataset.link);
+    const link=event.target.dataset.link;
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+})
