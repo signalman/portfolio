e604811c968ea5b47ec55b0b2a7b46975378f1a6
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-04-27 21:38:06 +0900

    js home_opacity

--- main.js
+++ main.js
@@ -5,13 +5,11 @@ const navbarHeight= navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=>{
     if(window.scrollY>navbarHeight){
         navbar.classList.add('navbar--dark');
-    
     }
     else{
         navbar.classList.remove('navbar--dark');
     }
 });
-
 
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -27,3 +25,12 @@ homeContact.addEventListener('click',(event)=>{
     const scrollTo= document.querySelector(link);
     scrollTo.scrollIntoView({behavior: "smooth"});
 })
+
+const home= document.querySelector('.home__container');
+const homeheight= home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', ()=>{
+    const homeopacity=1-window.scrollY/homeheight;
+    home.style.opacity = homeopacity;
+})
+
