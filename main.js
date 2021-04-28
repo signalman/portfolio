240704ee66e529c04369e5d600b3b4d11c63c6c6
apicad0307 <32051350+apicad0307@users.noreply.github.com>
2021-04-28 21:19:05 +0900

    js: arrow up btn

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=>{
     const link=event.target.dataset.link;
     const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    scrollTo.scrollIntoView({behavior: "smooth"});
 })
 
 const homeContact = document.querySelector('.home__contact');
@@ -34,3 +34,22 @@ document.addEventListener('scroll', ()=>{
     home.style.opacity = homeopacity;
 })
 
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
+const arrowUp = document.querySelector('.arrow-up');
+arrowUp.addEventListener('click', ()=>{
+    scrollIntoView('#home');
+})
+
+document.addEventListener('scroll',()=>{
+    if(window.scrollY>homeheight){
+        arrowUp.classList.add('visible');
+    }
+    else{
+        arrowUp.classList.remove('visible');
+    }
+
+})
